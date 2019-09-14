2754eea5c8a65003ab8c0d178a33a10dea4d1e9e
rodolfo <rodolfo_eliezer@hotmail.com>
2019-09-14 17:27:37 -0300

    funcionando

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/authenticate', (req, res) => {
+  const cert = req.connection.getPeerCertificate()
+  if (req.client.authorized) {
+    res.send(`Hello ${cert.subject.CN}, your certificate was issued by ${cert.issuer.CN}!`) 
+  } else if (cert.subject) {
+		res.status(403)
+       .send(`Sorry ${cert.subject.CN}, certificates from ${cert.issuer.CN} are not welcome here.`)
+  } else {
+		res.status(401)
+		   .send(`Sorry, but you need to provide a client certificate to continue.`)
+	}
+});
+
 module.exports = router;
